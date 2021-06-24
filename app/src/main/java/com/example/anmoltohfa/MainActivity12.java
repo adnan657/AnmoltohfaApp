2410099716b21c184fc0fab88440f3eecb692e2f
Dani <mcsf19a046@pucit.edu.pk>
2021-06-24 03:50:50 -0700

    This is my 2nd  commit and we compt work on 2nd buton

--- app/src/main/java/com/example/anmoltohfa/MainActivity12.java
+++ app/src/main/java/com/example/anmoltohfa/MainActivity12.java
@@ -6,26 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity12 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main12);
     }
-
-    public void firstmove(View view) {
-        Intent intent=new Intent(this,MainActivity2.class);
+    public void Nextmove(View view) {
+        Intent intent=new Intent(this,MainActivity13.class);
         startActivity(intent);
     }
 
-    public void secondmove(View view) {
+    public void prevmove(View view) {
         Intent intent=new Intent(this,MainActivity11.class);
-        startActivity(intent);
-    }
-
-    public void thirdmove(View view) {
-        Intent intent=new Intent(this,MainActivity21.class);
         startActivity(intent);
     }
 }
