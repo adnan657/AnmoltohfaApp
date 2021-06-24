1245fef100811f93faee62beafc1793ff7be5794
Dani <mcsf19a046@pucit.edu.pk>
2021-06-24 04:09:16 -0700

    This is my 3rd  commit and we complt work on 3rd buton

--- app/src/main/java/com/example/anmoltohfa/MainActivity22.java
+++ app/src/main/java/com/example/anmoltohfa/MainActivity22.java
@@ -2,7 +2,9 @@ package com.example.anmoltohfa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity22 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity22 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
+    }
+    public void Nextmove(View view) {
+        Intent intent=new Intent(this,MainActivity23.class);
+        startActivity(intent);
+    }
+
+    public void prevmove(View view) {
+        Intent intent=new Intent(this,MainActivity21.class);
+        startActivity(intent);
     }
 }
